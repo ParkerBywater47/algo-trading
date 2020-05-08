c1179098922cd9b01c682f13d55ff1ec1b0d48ce
ParkerBywater47 <parker.bywater47@gmail.com>
2020-05-07 23:50:41 -0600

    restructured some things

--- src/trade_driver.py
+++ src/trade_driver.py
@@ -6,8 +6,7 @@ import sma
 
 
 last_trade_time = time.time() - 4000 # wow Parker.. A global and a magic number? 
-previous_periods = []
-sma_length = 5
+previous_periods = [10025.35, 9997.99, 9915.86, 9943.57, 9929.65]
 
 
 def on_message(ws, message):
@@ -17,17 +16,14 @@ def on_message(ws, message):
     try: 
         resp = json.loads(message)
         if time.time() >= last_trade_time + 3600: 
-            if len(previous_periods) < sma_length: 
-                previous_periods.append(resp['price'])
-                print(previous_periods) 
-#        f = open("../data/BTC_realtime_data.csv", "a")
-#        f.write(str(resp['price']) + "," + resp['time'][resp['time'].find("T") + 1:resp['time'].find(".")] + "\n")
-#        f.close()
+            algos.sma(resp['price'], previous_periods)
+                         
+
         print(resp['price'])
         
     except Exception as ex:
         print(ex)
-        print("It's probably okay if you just saw a JSON parse error or something similar")
+        print(previous_periods)
 
 
 def on_open(ws):
@@ -42,19 +38,10 @@ def on_open(ws):
 #        time.sleep(1)
     thread.start_new_thread(run, ())
 
-def on_error(ws, error):
-    print(error)
-
-def on_close(ws):
-    print("### closed ###")
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
-#    ws = websocket.WebSocketApp("ws://echo.websocket.org/",
-    ws = websocket.WebSocketApp("wss://ws-feed.pro.coinbase.com",
-                              on_message = on_message,
-                              on_error = on_error,
-                              on_close = on_close)
+    ws = websocket.WebSocketApp("wss://ws-feed.pro.coinbase.com", on_message = on_message)
     ws.on_open = on_open
     ws.run_forever()
 
