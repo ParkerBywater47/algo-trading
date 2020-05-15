4524c29879ccb2d770e47fb67aefc176dc8afc22
ParkerBywater47 <parker.bywater47@gmail.com>
2020-05-14 22:53:39 -0600

    uneventful commit

--- src/btc_trade_driver.py
+++ src/btc_trade_driver.py
@@ -5,8 +5,8 @@ from Sma import Sma
 
 def main():
     starting_capital = 445                                      # This will become a Coinbase API call eventually
-    an_hour = 3600                                              # an hour in seconds for code readablility
-    algo = Sma(bought=False, coin_balance=0, cash_balance=450, previous_periods=[8827.625, 8930.66, 8922.955, 8943.905], logfile_path="logs/btc_trade_driver.log") 
+    an_hour = 3600                                              # an hour in seconds forcode readablility
+    algo = Sma(bought=False, coin_balance=0, cash_balance=450, previous_periods=[9649.29, 9703.66, 9636.76], logfile_path="logs/btc_trade_driver.log") 
     while True:
         algo.run()  
         time.sleep(3600) 
