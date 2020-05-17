aa497d1ee0bfb521aca086c1624092ec2317a1a6
ParkerBywater47 <parker.bywater47@gmail.com>
2020-05-16 20:48:43 -0600

    updated to make compatible with changes to Sma.py

--- src/btc_trade_driver.py
+++ src/btc_trade_driver.py
@@ -4,9 +4,8 @@ from Sma import Sma
 
 
 def main():
-    starting_capital = 445                                      # This will become a Coinbase API call eventually
     an_hour = 3600                                              # an hour in seconds forcode readablility
-    algo = Sma(bought=False, coin_balance=0, cash_balance=starting_capital, previous_periods=[9395.69, 9375.22, 9277.36], logfile_path="logs/btc_trade_driver.log") 
+    algo = Sma(bought=False, previous_periods=[9422.145, 9458.005, 9540.00], logfile_path="logs/btc_trade_driver.log") 
     while True:
         algo.run()  
         time.sleep(3600) 
