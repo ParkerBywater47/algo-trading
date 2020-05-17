b687c2c343eca3cbcc3665cb1b610a938c85f5fc
ParkerBywater47 <parker.bywater47@gmail.com>
2020-05-16 20:50:00 -0600

    removed some unused garbage and a few convenience functions

--- src/coinbase_api.py
+++ src/coinbase_api.py
@@ -17,58 +17,35 @@ else:
     API_PASS = key_file.readline().strip()
 key_file.close()
 
-def make_coinbase_call(method, path, req_body='', get_params=None): 
-    """
-    :method is HTTP method as str
-    :path is the path to resource as str  
-    :req_body is the body of the request if needed. Instance of dict
-    """
-    timestamp = str(time.time())
-    if get_params is None:
-        headers = {
-                'CB-ACCESS-SIGN': sign(method, path, API_SECRET,  timestamp, req_body), 
-                'CB-ACCESS-TIMESTAMP': timestamp,
-                'CB-ACCESS-KEY': API_KEY,
-                'CB-ACCESS-PASSPHRASE': API_PASS,
-                'Content-Type': 'application/json', 
-                }
-    else: 
-        path_with_params = path_builder(path, get_params)  
-        headers = {
-                'CB-ACCESS-SIGN': sign(method, path_with_params, API_SECRET,  timestamp, req_body), 
-                'CB-ACCESS-TIMESTAMP': timestamp,
-                'CB-ACCESS-KEY': API_KEY,
-                'CB-ACCESS-PASSPHRASE': API_PASS,
-                'Content-Type': 'application/json'
-                }
 
-    if method == "GET": 
-        resp = requests.get(ENDPOINT + path, headers=headers)
-    elif method == "POST":
-        resp = requests.post(ENDPOINT + path, headers=headers, json=req_body)
-    else: 
-        print("HTTP method '" + method + "' not supported")
-        sys.exit(1)
-
-    return resp.json()
+def get_BTC_balance():
+    accounts_get = coinbase_GET("/accounts")    
     
-#    if isinstance(resp.json(), list) and len(resp.json()) > 0:
-#        # use json.dumps because Python prints with fucking single quotes
-#        print(str(json.dumps(resp.json()))[1:-1])
-#    else: 
-#        print(json.dumps(resp.json()))
+    # search for BTC in the list
+    for i in accounts_get:
+        if i["currency"] == "BTC": 
+            return float(i["available"])    
+
+
+def get_USDC_balance():
+    accounts_get = coinbase_GET("/accounts")    
+    
+    # search for USDC in the list
+    for i in accounts_get:
+        if i["currency"] == "USDC": 
+            return float(i["available"])    
 
 
 def coinbase_GET(path, request_body=''): 
     timestamp = str(time.time())
     headers = {
-            'CB-ACCESS-SIGN': sign('POST', path, API_SECRET,  timestamp, request_body), 
+            'CB-ACCESS-SIGN': sign('GET', path, API_SECRET,  timestamp, request_body), 
             'CB-ACCESS-TIMESTAMP': timestamp,
             'CB-ACCESS-KEY': API_KEY,
             'CB-ACCESS-PASSPHRASE': API_PASS,
             'Content-Type': 'application/json', 
             }
-    if request_body != "": 
+    if request_body == "": 
         resp = requests.get(ENDPOINT + path, headers=headers)
     else: 
         resp = requests.get(ENDPOINT + path, headers=headers, json=request_body)
@@ -105,22 +82,11 @@ def coinbase_DELETE(path, request_body=''):
 
 
 def main(): 
-    """ a test main """
-    current_price = float(requests.get("https://api.coinbase.com/v2/exchange-rates?currency=BTC").json()['data']['rates']['USD'])
-    ask_price = int(current_price * 1.0025 * 100) / 100
-    stop_loss_price = int(current_price * .925 * 100) / 100
-    order = {
-        'size': 0.001,
-        'price': ask_price, 
-        'side': 'buy',
-        'product_id': 'BTC-USDC',
-        'stop':'loss', 
-        'stop_price': stop_loss_price, 
-    }
-    #print(json.dumps(make_coinbase_call('GET', "/orders"), sort_keys=True, indent=4))
-    #print(json.dumps(coinbase_POST("/orders", order), sort_keys=True, indent=4))
-    print(json.dumps(coinbase_DELETE("/orders/18ea5f96-9429-430e-99ef-8467ff351e0f"), sort_keys=True, indent=4))
-    #print(json.dumps(make_coinbase_call('GET', "/currencies"), sort_keys=True, indent=4))
+    """for tests and such"""
+    print(get_BTC_balance())
+    print(get_USDC_balance())
+    print(get_price())
+    print(json.dumps(coinbase_GET("/products/BTC-USDC/ticker"), sort_keys=True, indent=4))
 
 
 def sign(method, path,  secret, timestamp, req_body=""): 
@@ -132,7 +98,7 @@ def sign(method, path,  secret, timestamp, req_body=""):
 
 
 def get_price(): 
-    """ return the price to a precision of two decimal points cuz coi boi api """ 
+    """ return the price to a precision of two decimal points because Coinbase requires this """ 
     return float(requests.get("https://api.coinbase.com/v2/exchange-rates?currency=BTC").json()['data']['rates']['USD'])
 
 
@@ -145,6 +111,7 @@ def path_builder(path, get_params):
     for key in get_params.keys():
         out += "?" + key + "=" + get_params[key] + "&"
     return out
+
 
 
 
