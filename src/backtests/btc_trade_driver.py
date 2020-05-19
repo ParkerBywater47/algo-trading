b03582159090487f001fd48d4715821029a4c96f
ParkerBywater47 <parker.bywater47@gmail.com>
2020-05-18 20:14:41 -0600

    uneventful change

--- src/backtests/btc_trade_driver.py
+++ src/backtests/btc_trade_driver.py
@@ -5,10 +5,10 @@ from Sma import Sma
 
 def main():
     an_hour = 3600                                              # an hour in seconds forcode readablility
-    algo = Sma(bought=False, previous_periods=[9422.145, 9458.005, 9540.00], logfile_path="logs/btc_trade_driver.log") 
+    algo = Sma(bought=False, previous_periods=[9422.145, 9458.005, 9540.00], logfile_path="/dev/null") 
     while True:
         algo.run()  
-        time.sleep(3600) 
+        time.sleep(120) 
 
 
 if __name__ == "__main__": 
