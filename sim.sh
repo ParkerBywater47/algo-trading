aed8261d0cbee19e51793518f35b8a5e37bec520
ParkerBywater47 <parker.bywater47@gmail.com>
2020-06-24 01:23:17 -0600

    Documented some testing results

--- sim.sh
+++ sim.sh
@@ -1,15 +1,13 @@
-data_dir="long-backtest-data/"
-#tickers="COKE HAL KOS MRO RCL SLB UCO USO WFC XOM CSCO CCL GILD"
-tickers="HAL"
+data_dir="data/dataset1/"
+tickers="CCL KOS XOM MRO RCL SLB UCO USO WFC CSCO GILD COKE HAL"
+tickers="CCL"
 hist_suffix="_h.csv"
 sim_suffix="_s.csv"
-out_path="testing-results/"
+out_path="testing-results/dataset1/"
 fuck_bash="_para_raw.txt"
 
 for ticker in $tickers; do 
     echo $ticker; 
-    ./dynema $data_dir$ticker$hist_suffix $data_dir$ticker$sim_suffix; 
-    backtest.py dynema $data_dir$ticker$(echo ".csv"); 
-    echo
-#    ./parallel_find_optimal_dynema.out $data_dir$ticker$hist_suffix $data_dir$ticker$sim_suffix > $out_path$ticker$fuck_bash;
+    ./find_optimal_dynema.out $data_dir$ticker$(echo 4)$hist_suffix $data_dir$ticker$(echo 4)$sim_suffix > $out_path$ticker$fuck_bash; 
+    #backtest.py dynema $data_dir$ticker$(echo ".csv"); 
 done; 
---
+++ testing-results/README.md
@@ -0,0 +1,6 @@
+# Brief explanation of what this directory contains
+* `dataset*` directories hold results for running [this](../src/backtests/find_optimal_dynema.cpp) code on the historical data in the `data/dataset*` directory. 
+    
+* `process.sh` is a script that sorts the output data by performance (the first column of the \<ticker\>]\_raw.txt file)
+
+
