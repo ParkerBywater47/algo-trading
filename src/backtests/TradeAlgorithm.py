f01e84f97b30223185a4ce77d6448985007bc01e
ParkerBywater47 <parker.bywater47@gmail.com>
2020-06-29 23:24:17 -0600

    removed some garbage

--- src/backtests/TradeAlgorithm.py
+++ src/backtests/TradeAlgorithm.py
@@ -5,8 +5,3 @@ class TradeAlgorithm:
     def run(self):
         raise NotImplementedError("TradeAlgorithm is an abstract base class and should not be instantiated")
 
-    def average(self, lst): 
-        the_sum = 0
-        for i in lst: 
-            the_sum += i
-        return the_sum / len(lst)
