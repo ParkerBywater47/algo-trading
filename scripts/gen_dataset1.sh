794c79fbccc968bf0c9450223326cae717a478f6
ParkerBywater47 <parker.bywater47@gmail.com>
2020-07-10 16:02:52 -0600

    fixed a date in a comment

--- scripts/gen_dataset1.sh
+++ scripts/gen_dataset1.sh
@@ -4,7 +4,7 @@ sim_suffix="_s.csv"
 out_dir="../data/dataset1/"
 base_link="https://query1.finance.yahoo.com/v7/finance/download/" 
 
-# These GET parameters correspond to data up to 7/19/20 (inclusive)
+# These GET parameters correspond to data up to 6/19/20 (inclusive)
 hists=("?period1=1529452800&period2=1560988800&interval=1d&events=history" "?period1=1497916800&period2=1529452800&interval=1d&events=history" "?period1=1466380800&period2=1497916800&interval=1d&events=history" "?period1=1434758400&period2=1466380800&interval=1d&events=history" "?period1=1403222400&period2=1434758400&interval=1d&events=history")
 sims=("?period1=1560988800&period2=1592611200&interval=1d&events=history" "?period1=1529452800&period2=1592611200&interval=1d&events=history" "?period1=1497916800&period2=1592611200&interval=1d&events=history" "?period1=1466380800&period2=1592611200&interval=1d&events=history" "?period1=1434758400&period2=1592611200&interval=1d&events=history")
 
