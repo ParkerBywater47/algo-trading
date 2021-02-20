ede22f01757a31653f7befbb895bfc1504c19ac8
Parker Bywater <parker.bywater47@gmail.com>
2021-02-20 13:55:22 -0700

    added code to fetch price data automatically at btc trade driver launch

--- src/live/btc_trade_driver.py
+++ src/live/btc_trade_driver.py
@@ -1,11 +1,16 @@
 import time, sys
 from TradeAlgorithm import TradeAlgorithm
 from Sma import Sma
+from coinbase_api import get_historic_rates
 
 
 def main():
     sleep_time_in_seconds = 3599.850
-    # TODO figure out Coinbase garbage to automatically fetch previous periods
+
+    # this automatically fetches recent price data, currently in testing only 
+    prev = get_historic_rates()
+    print("auto fetch of historic rates:", prev)
+
     algo = Sma(bought=False, previous_periods=[44636.56, 44983.82, 45161.88], logfile_path="logs/btc_trade_driver.log")
     while True:                                
         algo.run()                             
--- src/live/coinbase_api.py
+++ src/live/coinbase_api.py
@@ -1,4 +1,5 @@
 import json, hashlib, time, requests, base64, hmac, sys
+from datetime import datetime, timedelta
 
 
 SANDBOX_ENDPOINT = "https://api-public.sandbox.pro.coinbase.com"
@@ -111,6 +112,36 @@ def get_price(ticker="BTC-USDC"):
 def get_products(): 
     return coinbase_GET("/products")
 
+
+def get_historic_rates(start=None, end=None, periods=3, granularity=3600): 
+    """ 
+    format of data returned from Coinbase is 
+        [[UNIX timestamp, low, high, open, close, volume]...]
+    sorted in descending order by timestamp
+    """
+    close = 4 # see above docstring
+    end = datetime.now() - timedelta(hours=1)
+    start = end - timedelta(hours=periods)
+    request_body = {
+        "granularity": granularity , 
+        "start": start.isoformat(), 
+        "end": end.isoformat(),
+    }
+    candles = coinbase_GET("/products/BTC-USDC/candles", request_body=request_body)
+
+    # TODO pretending that we always get enough data which isn't guaranteed, but unlikely anyhow
+    if len(candles) > 2: 
+        actual_granularity = candles[0][0] - candles[1][0] # called actual_granularity because coinbase 
+                                                     # doesn't give the requested granularity if 
+                                                     # "enough data is readily available"
+
+        adj = int(granularity / actual_granularity) 
+        adj_periods = (periods + 1) * adj # * int(granularity / actual_granularity)
+#        old = candles[:adj_periods:adj]
+        out = [candles[x][close] for x in range(adj, (periods + 1) * adj, adj)]
+        return out
+           
+    
 
 # This should not be used, but I left the code here because I don't want to go find it again if I need it
 #def public_price():
